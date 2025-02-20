d1e47a457181b57ed95f42e98c641b09a48b69d1
MukulWaval <MukulWaval@users.noreply.github.com>
2025-02-20 16:46:50 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="MukulWaval"
+original_name="zenith"
+original_urlname="zenith"
+original_description="Awesome zenith created by MukulWaval"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv zenith $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
